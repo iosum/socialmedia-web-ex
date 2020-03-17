da509b2a2346aa177bff6893c66571afa1ce6b48
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-03-17 13:24:57 -0400

    Implement Content.Publish (use case 1)

--- Test/Content.cs
+++ Test/Content.cs
@@ -6,11 +6,25 @@ namespace Test
     internal class Content
     {
         // create a collection containing user and posts and instantiate to the default
-        internal Dictionary<User, Post> PublishPosts { get; set; } = new Dictionary<User, Post>();
+        // as one user can have many post, so we a list of posts
+        internal Dictionary<User, List<Post>> PublishPosts { get; set; } = new Dictionary<User, List<Post>>();
 
         internal void Publish(User user, Post post)
         {
-            PublishPosts.Add(user, post);
+            // if the user exists
+            if(PublishPosts.ContainsKey(user))
+            {
+                // add the post to the existing collection
+                PublishPosts[user].Add(post);
+
+            }
+            else
+            {
+                // create a new list of post and add the user
+                var posts = new List<Post> { post };
+                PublishPosts.Add(user, posts);
+            }
+            
         }
     }
 }
--- Test/Tests_SocialMedia.cs
+++ Test/Tests_SocialMedia.cs
@@ -11,15 +11,18 @@ namespace Test
         {
             // need a id for identify the user
             var user = new User(1);
-            var post = new Post("The Content");
+            var post1 = new Post("One");
+            var post2 = new Post("Two");
 
             // content managing the association between user and post
             var content = new Content();
-            content.Publish(user, post);
+            content.Publish(user, post1);
+            content.Publish(user, post2);
+
 
             Check.That(content.PublishPosts.Keys).CountIs(1);
             Check.That(content.PublishPosts.Keys).Contains(user);
-            Check.That(content.PublishPosts.Values).Contains(post);
+            Check.That(content.PublishPosts[user]).Contains(post1, post2);
         }
     }
 }
